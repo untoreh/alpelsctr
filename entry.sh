57570cd3a6258573ed2f95250a3f55ff6f03d6aa
natostanco <bassobassista@gmail.com>
2016-04-29 13:25:56 +0200

    .

--- entry.sh
+++ entry.sh
@@ -13,6 +13,11 @@ if [ "$1" = '/els/bin/elasticsearch' -a "$(id -u)" = '0' ]; then
 	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
 	chown -R elasticsearch:elasticsearch /els
 	
+	ln -s /els/vol/data /els/data
+	ln -s /els/vol/config /els/config
+	ln -s /els/vol/plugins /els/plugins
+	ln -s /els/vol/logs /els/logs
+	
 	set -- gosu elasticsearch "$@"
 	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
 fi
